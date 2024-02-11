748bf5eff79d6dd2aa808a3f76421f96292c954b
Umutoni26 <vanessaumutoni26@gmail.com>
2024-02-10 16:59:50 -0800

    baby store

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'50px'
+        
+
+      }
+    }
+  },
+
+
+  plugins: [],
+}
+
